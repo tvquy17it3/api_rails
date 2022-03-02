27933eaad8b7fe3cdd6e3caa6b9febd7afa762a7
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2022-03-02 22:52:29 +0700

    enum, scope

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,4 +1,19 @@
 class Timesheet < ApplicationRecord
+  before_create :create_check_in
   belongs_to :user
   has_many :timesheet_details, dependent: :destroy
+
+  scope :with_checkin_today, ->(id) {
+    where(check_in: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    .where(user_id: id)}
+
+  enum status: [:notcheckout, :checkout], _prefix: true
+
+  private
+  def create_check_in
+    self.check_in = Time.current
+    self.check_out = Time.current
+    self.hours = 0
+    self.status = false
+  end
 end
