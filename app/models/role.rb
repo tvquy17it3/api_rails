a29a77f8c233a5dd1def7fa03b22df43d25cd575
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2022-06-06 08:55:01 +0700

    edit login and roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
   has_many :users, dependent: :destroy
   acts_as_paranoid
-  enum slug: { admin_role: "admin", manager_role: "manager", user_role: "user" }
+  enum slug: { admin_role: :admin, manager_role: :manager, user_role: :user }
 end
